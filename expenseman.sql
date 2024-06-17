24a7a92e54709533939d85f0ad9249943e2acf6c
Nandani Ramoliya <142075206+NandaniRamoliya@users.noreply.github.com>
2024-06-17 21:10:21 +0530

    Create expenseman.sql

---
+++ expenseman.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- Generation Time: May 15, 2024 at 11:47AM
+
+-- Database: `expenseman`
+------------------------------------------------------------
+
+-- Table structure for table `budget`
+CREATE TABLE IF NOT EXISTS `budget` (
+  `ID` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `Budget` decimal(25,2) NOT NULL,
+  `RDATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  ) 
+
+-- Dumping data for table `budget`
+INSERT INTO `budget` (`ID`, `UserId`, `Budget`, `RDATE`) VALUES
+(3, 2, '7895.00', '2024-05-15 04:50:46');
+----------------------------------------------------------
+
+-- Table structure for table `expense`
+CREATE TABLE IF NOT EXISTS `expense` (
+  `UserId` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `Item` varchar(255) NOT NULL,
+  `Cost` decimal(25,2) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  ) 
+
+-- Dumping data for table `expense`
+INSERT INTO `expense` (`UserId`, `ID`, `Item`, `Cost`, `Date`) VALUES
+(2, 2, 'Grocery', '196.00', '2024-05-15 13:56:00'),
+(2, 4, 'Test Yesterday', '699.00', '2024-05-15 17:55:00'),
+(2, 5, 'T220', '2000.00', '2024-05-15 04:52:00');
+----------------------------------------------------------
+
+-- Table structure for table `user`
+CREATE TABLE IF NOT EXISTS `user` (
+  `Full_Name` varchar(40) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `Password` varchar(32) NOT NULL,
+  `Photo` varchar(255) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  ) 
+
+-- Dumping data for table `user`
+INSERT INTO `user` (`Full_Name`, `Email`, `Username`, `UserId`, `Password`, `Photo`, `RegDate`) VALUES
+('Nandani Ramoliya', 'nandaniramoliya@mail.com', 'nandani', 1, '5f4dcc3b5aa765d61d8327deb882cf99', '../static/images/userlogo.png', '2024-05-15 12:59:25'),
+('Khushi Rao', 'raokhushi@mail.com', 'khushi', 2, '5f4dcc3b5aa765d61d8327deb882cf99', '../static/images/userlogo.png', '2024-05-15 09:16:13');
+------------------------------------------------------------
+
+------------------Indexes for dumped tables-----------------------
+-- Indexes for table `budget`
+ALTER TABLE `budget` ADD PRIMARY KEY (`ID`);
+
+-- Indexes for table `expense`
+ALTER TABLE `expense` ADD PRIMARY KEY (`ID`);
+
+-- Indexes for table `user`
+ALTER TABLE `user` ADD PRIMARY KEY (`UserId`);
+
+-----------------AUTO_INCREMENT for dumped tables-----------------
+-- AUTO_INCREMENT for table `budget`
+ALTER TABLE `budget`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+
+-- AUTO_INCREMENT for table `expense`
+ALTER TABLE `expense`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+
+-- AUTO_INCREMENT for table `user`
+ALTER TABLE `user`
+MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
